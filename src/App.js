7caa42fd80d716762876328b8b88a0f36d857326
Kyle Weisse <kyle.weisse@gmail.com>
2021-06-17 13:23:10 -0400

    why isn't this working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ReactAudioPlayer from 'react-audio-player';
 
@@ -6,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ReactAudioPlayer src="../public/music/follow-your-arrow.mp3" autoPlay controls />
+        <ReactAudioPlayer src="../public/music/follow-your-arrow.mp3" controls/>
         <a
           className="App-link"
           href="https://reactjs.org"
